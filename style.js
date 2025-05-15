3eb3cfa1ef11b3d414e5d94f34572d6166b09624
Rajshree Balsamant <rajshreebalsamant@gmail.com>
2025-05-15 21:45:29 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+document.querySelector(".fa-bars").addEventListener("click", function() {
+  document.querySelector("nav ul").style.right = "0";
+});
+
+document.querySelector(".fa-times").addEventListener("click", function() {
+  document.querySelector("nav ul").style.right = "-200px";
+});
