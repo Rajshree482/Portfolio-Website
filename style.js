d6549060338d4ef54842d3707a417978acfca15b
Rajshree Balsamant <rajshreebalsamant@gmail.com>
2025-08-21 19:38:44 +0530

    Delete style.js

--- style.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".fa-bars").addEventListener("click", function() {
-  document.querySelector("nav ul").style.right = "0";
-});
-
-document.querySelector(".fa-times").addEventListener("click", function() {
-  document.querySelector("nav ul").style.right = "-200px";
-});
